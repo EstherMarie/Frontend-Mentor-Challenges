145c15ac4bfc930eef7dc17234de12b2f0ee7ed1
Esther Marie <24815585+EstherMarie@users.noreply.github.com>
2022-06-12 00:10:39 -0300

    Initial config: advice generator app

---
+++ advice-generator-app-main/src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="sr-only">Addvice Generator App</h1>
+      <div className="attribution">
+        Challenge by{' '}
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by{' '}
+        <a
+          href="https://github.com/EstherMarie"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Esther Marie
+        </a>
+        .
+      </div>
+    </div>
+  );
+}
+
+export default App;
