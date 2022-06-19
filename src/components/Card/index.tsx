af819bca77cc2708157b0ed648d5909a76af7e39
Esther Marie <24815585+EstherMarie@users.noreply.github.com>
2022-06-19 19:34:16 -0300

    feat(card, api): fetch random advice from API

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,22 +1,47 @@
-import React from 'react';
-import Image from 'next/image';
-import styles from './Card.module.scss';
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import styles from "./Card.module.scss";
 
-interface CardProps {
-	number: number;
-	text: string;
+interface ApiData {
+  slip: {
+    id: number;
+    advice: string;
+  };
 }
 
-export default function Card({ number, text }: CardProps) {
-	return (
-		<div className={styles.card}>
-			<div className={styles.advice}>ADVICE #{number}</div>
+export default function Card() {
+  const [data, setData] = useState<ApiData>();
 
-			<p className={styles.quote}>{text}</p>
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-			<button type="submit" className={styles.btn}>
-				<Image src="/advice-generator/images/icon-dice.svg" alt="New Advice" width={24} height={24} />
-			</button>
-		</div>
-	);
+  function fetchData() {
+    fetch("https://api.adviceslip.com/advice", {
+      cache: "no-cache",
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        setData(data);
+      });
+  }
+
+  return (
+    <div className={styles.card}>
+      <div className={styles.advice}>ADVICE #{data?.slip.id}</div>
+
+      <p className={styles.quote}>{data?.slip.advice}</p>
+
+      <button type="submit" onClick={() => fetchData()} className={styles.btn}>
+        <Image
+          src="/advice-generator/images/icon-dice.svg"
+          alt="New Advice"
+          width={24}
+          height={24}
+        />
+      </button>
+    </div>
+  );
 }
--- src/pages/advice-generator-app.tsx
+++ src/pages/advice-generator-app.tsx
@@ -1,20 +1,17 @@
-import Head from 'next/head';
-import Card from '../components/Card';
+import Head from "next/head";
+import Card from "../components/Card";
 
 export default function AdviceGeneratorApp() {
-	return (
-		<>
-			<Head>
-				<title>Frontend Mentor | Advice generator app</title>
-			</Head>
-			<main>
-				<div className="container">
-					<Card
-						number={114}
-						text="It is easy to sit up and take notice, what's difficult is getting up and taking action."
-					/>
-				</div>
-			</main>
-		</>
-	);
+  return (
+    <>
+      <Head>
+        <title>Frontend Mentor | Advice generator app</title>
+      </Head>
+      <main>
+        <div className="container">
+          <Card />
+        </div>
+      </main>
+    </>
+  );
 }
