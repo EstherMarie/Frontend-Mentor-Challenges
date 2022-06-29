3a4f9939a508f63262016f3a9a7c9ce8475164b5
Esther Marie Reis <24815585+EstherMarie@users.noreply.github.com>
2022-06-29 20:18:47 -0300

    refactor: add h1 to advice generator app

--- src/pages/advice-generator-app.tsx
+++ src/pages/advice-generator-app.tsx
@@ -8,6 +8,7 @@ export default function AdviceGeneratorApp() {
         <title>Frontend Mentor | Advice generator app</title>
       </Head>
       <main>
+        <h1 className="sr-only">Advice Generator App</h1>
         <div className="container">
           <Card />
         </div>
