ef1f7dff4ba332156cec15bb37c62fb1a2b9bc8c
Esther Marie <24815585+EstherMarie@users.noreply.github.com>
2022-07-01 21:33:00 -0300

    feat (styles): add themes.ts

---
+++ src/styles/themes.ts
@@ -0,0 +1,20 @@
+export const adviceGeneratorTheme = {
+  theme: {
+    colors: {
+      primary: {
+        lightCyan: 'hsl(193, 38%, 86%)',
+        neonGreen: 'hsl(150, 100%, 66%)',
+      },
+      neutral: {
+        grayishBlue: 'hsl(217, 19%, 38%)',
+        darkGrayishBlue: 'hsl(217, 19%, 24%)',
+        darkBlue: 'hsl(218, 23%, 16%)',
+      },
+    },
+    typography: {
+      fontSize: '2.8rem',
+      fontFamily: '"Manrope", sans-serif',
+      fontWeight: 800,
+    },
+  },
+};
