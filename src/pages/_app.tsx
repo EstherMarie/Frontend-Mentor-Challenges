3991279808467a6b593aa8394f91d58be3fe4363
Esther Marie <24815585+EstherMarie@users.noreply.github.com>
2022-07-02 21:49:42 -0300

    feat: dynamicaly change themes using next/router

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,31 @@
-import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from '../styles/globals';
+import * as Themes from '../styles/themes';
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+  const router = useRouter();
+  let pathname = router.pathname;
+
+  let actualTheme =
+    pathname == '/'
+      ? 'HomeTheme'
+      : pathname
+          .replace('/', '')
+          .split('-')
+          .map((word) => word[0].toUpperCase() + word.substring(1))
+          .join('')
+          .concat('Theme');
+
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={Themes[actualTheme]}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default MyApp;
--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -1,4 +1,5 @@
-export const adviceGeneratorTheme = {
+// TODO: create style guide for Homepage
+export const HomeTheme = {
   theme: {
     colors: {
       primary: {
@@ -19,7 +20,28 @@ export const adviceGeneratorTheme = {
   },
 };
 
-export const easyBankTheme = {
+export const AdviceGeneratorAppTheme = {
+  theme: {
+    colors: {
+      primary: {
+        lightCyan: 'hsl(193, 38%, 86%)',
+        neonGreen: 'hsl(150, 100%, 66%)',
+      },
+      neutral: {
+        grayishBlue: 'hsl(217, 19%, 38%)',
+        darkGrayishBlue: 'hsl(217, 19%, 24%)',
+        darkBlue: 'hsl(218, 23%, 16%)',
+      },
+    },
+    typography: {
+      fontSize: '2.8rem',
+      fontFamily: '"Manrope", sans-serif',
+      fontWeight: 800,
+    },
+  },
+};
+
+export const EasybankLandingPageTheme = {
   theme: {
     colors: {
       primary: {
