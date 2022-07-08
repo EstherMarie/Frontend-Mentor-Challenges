b31518f181ef655727eecef2d3e7359bc2c40039
Esther Marie <24815585+EstherMarie@users.noreply.github.com>
2022-07-07 22:34:03 -0300

    refactor (easybank CTA): adjust padding and font

--- src/components/easybank/CallToAction/cta.styled.ts
+++ src/components/easybank/CallToAction/cta.styled.ts
@@ -2,22 +2,25 @@ import styled from 'styled-components';
 
 export const StyledCTA = styled.button`
   background: linear-gradient(274.17deg, ${({theme}) => theme.easybankLandingPage.colors.primary.brightCyan} 0%, ${({theme}) => theme.easybankLandingPage.colors.primary.limeGreen} 100%);
-  border-radius: 20px;
+  border-radius: 25px;
   border: none;
 
-  padding: 10px 29px;
+  padding: 14px 35px;
 
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
 
+  font-size: 14px;
+  font-weight: ${({ theme }) => theme.easybankLandingPage.typography.fontWeight.bold};
+  font-style: normal;
+  line-height: 16px;
+  font-family: ${({ theme }) => theme.easybankLandingPage.typography.fontFamily};
   color: ${({theme}) => theme.easybankLandingPage.colors.neutral.white};
 
-  font-weight: 700;
-  font-size: 13.7px;
-
   transition: all 0.2s ease-in-out;
+
 
   &:hover,
   &:focus {
