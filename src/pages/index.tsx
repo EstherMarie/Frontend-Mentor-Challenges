2bbedb3532a7ca20743e454e0d64db152dc02155
Esther Marie <24815585+EstherMarie@users.noreply.github.com>
2022-07-13 23:13:04 -0300

    feat: create first components for Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,17 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Link from 'next/link';
+import { HomeMain } from '../components/Home/MainSection';
 
 const Home: NextPage = () => {
   return (
-    <div className='container'>
+    <div className="container">
       <Head>
         <title>Frontend Mentor Challenges</title>
-        <meta name='description' content='Projects from Frontend Mentor' />
+        <meta name="description" content="Projects from Frontend Mentor" />
       </Head>
 
-      <main>
-        <h1>Frontend Mentor Challenges</h1>
-        <ul>
-          <li>
-            <Link href='/advice-generator-app'>
-              <a>Advice Generator App</a>
-            </Link>
-            <Link href='/easybank-landing-page'>
-              <a>Easybank Landing Page</a>
-            </Link>
-          </li>
-        </ul>
-      </main>
+      <HomeMain />
     </div>
   );
 };
