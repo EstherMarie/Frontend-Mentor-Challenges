7d6ced8a5fb1b5823281ab53e8fce1eac5307b94
Esther Marie Reis <24815585+EstherMarie@users.noreply.github.com>
2022-08-18 21:43:36 -0300

    fix: fix theme key for home main styles

--- src/components/Home/MainSection/HomeMain.styled.ts
+++ src/components/Home/MainSection/HomeMain.styled.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
 
 export const Main = styled.main`
-  font-family: ${({ theme }) => theme.theme.typography.fontFamily};
+  font-family: ${({ theme }) => theme.home.typography.fontFamily};
 `;
