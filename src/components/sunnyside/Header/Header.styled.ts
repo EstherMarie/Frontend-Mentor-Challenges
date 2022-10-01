350b1c61f76617b8293c0a4fc6836323a8505b57
Esther Marie <24815585+EstherMarie@users.noreply.github.com>
2022-09-30 21:22:55 -0300

    refactor: header desktop and mobile

--- src/components/sunnyside/Header/Header.styled.ts
+++ src/components/sunnyside/Header/Header.styled.ts
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 export const Header = styled.header`
+  margin-top: 34px;
+  position: relative;
+
   .wrapper {
     --container-width: 1352px;
     --padding: 24px;
@@ -16,10 +19,33 @@ export const Header = styled.header`
     align-items: center;
   }
 
+  .header_logo {
+    position: relative;
+    width: 124px;
+    height: 24px;
+
+    @media (min-width: 768px) {
+      width: 170px;
+      height: 33px;
+    }
+  }
+
   nav {
+    ul {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 45px;
+    }
+
     a {
       font-family: ${({ theme }) =>
         theme.sunnysideLandingPage.typography.fontFamily.barlow};
+      font-style: normal;
+      font-weight: 700;
+      font-size: 18px;
+      line-height: 22px;
+      color: #ffffff;
     }
 
     .contact_link {
@@ -27,6 +53,28 @@ export const Header = styled.header`
         theme.sunnysideLandingPage.typography.fontFamily.fraunces};
       font-weight: ${({ theme }) =>
         theme.sunnysideLandingPage.typography.fontWeight.fraunces700};
+    }
+
+    @media (max-width: 768px) {
+      display: none;
+    }
+  }
+
+  .menu_icon {
+    background-color: transparent;
+    border: none;
+
+    position: relative;
+    width: 24px;
+    height: 18px;
+
+    @media (min-width: 768px) {
+      display: none;
+    }
+
+    &:hover,
+    &:active {
+      cursor: pointer;
     }
   }
 `;
