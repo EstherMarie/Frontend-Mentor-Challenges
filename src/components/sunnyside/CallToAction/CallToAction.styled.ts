9834bd400d6e62bc99a49075fbe21832ebe91753
Esther Marie <24815585+EstherMarie@users.noreply.github.com>
2022-09-30 21:22:27 -0300

    feat: add CallToAction Button

---
+++ src/components/sunnyside/CallToAction/CallToAction.styled.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+interface buttonProps {
+  buttonVariant: 'desktop' | 'mobile';
+}
+
+export const button = styled.button<buttonProps>`
+  font-family: ${({ theme }) =>
+    theme.sunnysideLandingPage.typography.fontFamily.fraunces};
+  font-style: normal;
+  font-weight: 700;
+  font-size: 15px;
+  line-height: 18px;
+  color: #000000;
+
+  width: 140px;
+  height: 56px;
+  border: transparent;
+  border-radius: 30px;
+
+  background-color: ${(props) =>
+    props.buttonVariant === 'desktop' ? '#ffffff' : '#FBD601'};
+  transition: all 0.2s ease-in-out;
+
+  &:hover,
+  &:active {
+    cursor: pointer;
+    filter: opacity(0.6);
+
+    /* 
+    Serious color-contrast problem
+
+    color: ${(props) =>
+      props.buttonVariant === 'desktop' ? '#ffffff' : '#000000'}; 
+    */
+  }
+`;
