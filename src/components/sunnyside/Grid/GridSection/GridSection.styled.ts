bad05e2760dedf87fb1cce8a20a2e3e737213080
Esther Marie <24815585+EstherMarie@users.noreply.github.com>
2022-12-11 18:09:00 -0300

    feat: add last row of grid component

--- src/components/sunnyside/Grid/GridSection/GridSection.styled.ts
+++ src/components/sunnyside/Grid/GridSection/GridSection.styled.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 interface StyledGridProps {
-  isReverse: boolean;
+  isReverse?: boolean;
 }
 
 interface StyledCTAProps {
@@ -27,12 +27,17 @@ export const GridSection = styled.div<StyledGridProps>`
 `;
 
 export const TextArea = styled.div`
+  height: 437px;
   padding: 63px 24px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 
   h2 {
     font-family: ${({ theme }) =>
       theme.sunnysideLandingPage.typography.fontFamily.fraunces};
-    font-weight: 700;
+    font-weight: 900;
     font-size: 34px;
     line-height: 39px;
     text-align: center;
@@ -84,7 +89,7 @@ export const TextArea = styled.div`
 export const LearnMoreCTA = styled.div<StyledCTAProps>`
   font-family: ${({ theme }) =>
     theme.sunnysideLandingPage.typography.fontFamily.fraunces};
-  font-weight: 700;
+  font-weight: 900;
   font-size: 17px;
   line-height: 49px;
   text-align: center;
@@ -139,5 +144,88 @@ export const ImageArea = styled.div`
   @media (min-width: 576px) {
     width: 50%;
     height: 600px;
+  }
+`;
+
+export const LastRowGrid = styled.div`
+  height: 601px;
+  padding-inline: 19px;
+
+  h2 {
+    font-family: ${({ theme }) =>
+      theme.sunnysideLandingPage.typography.fontFamily.fraunces};
+    font-weight: 900;
+    font-size: 28px;
+    line-height: 49px;
+    text-align: center;
+
+    margin-bottom: 24px;
+  }
+
+  p {
+    font-family: ${({ theme }) =>
+      theme.sunnysideLandingPage.typography.fontFamily.barlow};
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 27px;
+    text-align: center;
+    letter-spacing: -0.005em;
+  }
+
+  &:first-of-type {
+    background-image: url('/images/sunnyside/mobile/image-graphic-design.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+
+    .content {
+      margin-top: 391px;
+      /* color: #25564b; */
+      color: ${({ theme }) =>
+        theme.sunnysideLandingPage.colors.primary.darkDesaturatedCyan};
+    }
+  }
+
+  &:last-of-type {
+    background-image: url('/images/sunnyside/mobile/image-photography.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+
+    .content {
+      margin-top: 391px;
+      /* color: #19536b; */
+      color: ${({ theme }) =>
+        theme.sunnysideLandingPage.colors.primary.darkBlue};
+    }
+  }
+
+  @media (min-width: 576px) {
+    width: 50%;
+    height: 600px;
+
+    .content {
+      max-width: 337px;
+      margin-inline: auto;
+      margin-top: 390px;
+    }
+
+    h2 {
+      font-size: 28px;
+      line-height: 49px;
+    }
+
+    p {
+      font-size: 16px;
+      line-height: 27px;
+    }
+
+    &:first-of-type {
+      background-image: url('/images/sunnyside/desktop/image-graphic-design.jpg');
+    }
+
+    &:last-of-type {
+      background-image: url('/images/sunnyside/desktop/image-photography.jpg');
+    }
   }
 `;
--- src/components/sunnyside/Grid/index.tsx
+++ src/components/sunnyside/Grid/index.tsx
@@ -7,7 +7,7 @@ export function Grid() {
         title="Transform your brand"
         text="We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you."
         mobileImage="/images/sunnyside/mobile/image-transform.jpg"
-        DesktopImage="/images/sunnyside/desktop/image-transform.jpg"
+        desktopImage="/images/sunnyside/desktop/image-transform.jpg"
         isReverse={true}
         color="yellow"
       />
@@ -15,9 +15,10 @@ export function Grid() {
         title="Stand out to the right audience"
         text="Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we'll build and extend your brand in digital places."
         mobileImage="/images/sunnyside/mobile/image-stand-out.jpg"
-        DesktopImage="/images/sunnyside/desktop/image-stand-out.jpg"
+        desktopImage="/images/sunnyside/desktop/image-stand-out.jpg"
         color="softRed"
       />
+      <GridSection lastRow={true} />
     </>
   );
 }
