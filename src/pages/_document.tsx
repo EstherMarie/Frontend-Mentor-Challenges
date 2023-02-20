90eb85b78033f738b8fe3831fcd429e1b610d8de
Esther Marie <24815585+EstherMarie@users.noreply.github.com>
2023-02-20 12:53:27 -0300

    perf: preconnect fonts, preload easybank bg-image, add viewport meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -42,6 +42,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="author" content="Esther Marie" />
           <link
             rel="icon"
@@ -49,9 +50,23 @@ export default class MyDocument extends Document {
             sizes="32x32"
             href="/favicon-32x32.png"
           />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;800&family=Public+Sans:wght@300;400;700&family=Barlow:wght@300;400;600&family=Fraunces:wght@700;900&display=swap"
             rel="stylesheet"
+          />
+          <link 
+            rel="preload" 
+            as="image" 
+            href="/images/easybank-landing-page/bg-intro-mobile.svg" 
+            media='(max-width: 991px)'
+          />
+          <link 
+            rel="preload" 
+            as="image" 
+            href="/images/easybank-landing-page/bg-intro-desktop.svg" 
+            media='(min-width: 992px)'
           />
         </Head>
         <body>
